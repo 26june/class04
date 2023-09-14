dcae3cae9728d218b35438afb1dd2968af939bb9
26june <gj.abordo1@gmail.com>
2023-09-14 10:27:36 +0100

    add recursion

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 let name = prompt("What is your name");
 
+function addNoContent() {
+  document.write("<h1>I haven no food recommendations for you!</h1>");
+}
+
 function conditionalPasta() {
-  let pasta = prompt("Do you like pasta");
+  let pasta = prompt('Do you like pasta? Type "yes" or "no"');
 
   if (pasta === "no") {
     document.getElementById("yes-content").remove();
-  }
+
+    addNoContent();
+  } else if (pasta === "yes") {
+  } else conditionalPasta();
 }
