663b4343bf350e530edfae60c0c1130d5368ae7e
26june <gj.abordo1@gmail.com>
2023-09-14 10:45:13 +0100

    update functions

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-let name = prompt("What is your name");
+function addName() {
+  let name = prompt("What is your name");
+  document.write(name);
+}
 
 function addNoContent() {
   document.write("<h1>I haven no food recommendations for you!</h1>");
 }
 
-function conditionalPasta() {
-  let pasta = prompt('Do you like pasta? Type "yes" or "no"');
-
+function conditionalPasta(
+  pasta = prompt('Do you like pasta? Type "yes" or "no"')
+) {
   if (pasta === "no") {
     document.getElementById("yes-content").remove();
 
     addNoContent();
   } else if (pasta === "yes") {
-  } else conditionalPasta();
+  } else {
+    pasta = prompt(`Please type "yes" or "no"`);
+    conditionalPasta(pasta);
+  }
 }
